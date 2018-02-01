7df82da2ee3f6adb1255671fd7467460e2eacdfc
James Ferguson <ncjamesferguson@gmail.com>
2018-02-01 18:03:36 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,15 +1,15 @@
 class Show < ActiveRecord::Base
 
-def self.highest_rating
-  Show.maximum("rating")
-end
+  def self.highest_rating
+    Show.maximum("rating")
+  end
 
-def self.most_popular_show
-  self.order("rating DESC").limit(1)[0]
-end
+  def self.most_popular_show
+    self.order("rating DESC").limit(1)[0]
+  end
 
-def self.lowest_rating
-  Show.minimum("rating")
-end
+  def self.lowest_rating
+    Show.minimum("rating")
+  end
 
 end
